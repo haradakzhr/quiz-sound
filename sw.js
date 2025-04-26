70804914f5f9df0af1b1151a6c62f0132596aa7a
kazuharu.harada <kazuharu.harada.j4r@jp.denso.com>
2025-04-26 20:19:19 +0900

    test commit

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'sound-button-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './sounds/オールスター感謝祭.mp3',
+  './sounds/クイズ出題1.mp3',
+  './sounds/クイズ出題2.mp3',
+  './sounds/クイズ正解1.mp3',
+  './sounds/クイズ不正解1.mp3',
+  './sounds/ジャン！.mp3',
+  './sounds/チーン1.mp3',
+  './sounds/ドラムロール.mp3',
+  './sounds/ドンドンパフパフ.mp3',
+  './sounds/ラッパのファンファーレ.mp3',
+  './sounds/レベルアップ.mp3',
+  './sounds/歓声と拍手.mp3',
+  './sounds/試合終了のゴング.mp3',
+  './sounds/大勢で拍手.mp3',
+  './sounds/和太鼓でドドン.mp3',
+  './sounds/和太鼓でドン.mp3'
+];
